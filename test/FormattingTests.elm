d5cdf210996876f1eecd3bc538b7d5d41941a7d8
Kris Jenkins <krisajenkins@gmail.com>
2016-06-22 13:16:06 +0100

    Whitespace changes.

--- test/FormattingTests.elm
+++ test/FormattingTests.elm
@@ -16,9 +16,12 @@ tests =
 
 basicTests : Test
 basicTests =
-    [ assertEqual "Hello Kris!" (print (s "Hello " <> string <> s "!") "Kris")
-    , assertEqual "We need 5 cats." (print (s "We need " <> int <> s " cats.") 5)
-    , assertEqual "Height: 1.72" (print (s "Height: " <> float) 1.72)
+    [ assertEqual "Hello Kris!"
+        (print (s "Hello " <> string <> s "!") "Kris")
+    , assertEqual "We need 5 cats."
+        (print (s "We need " <> int <> s " cats.") 5)
+    , assertEqual "Height: 1.72"
+        (print (s "Height: " <> float) 1.72)
     ]
         |> List.map defaultTest
         |> ElmTest.suite "Basics"
